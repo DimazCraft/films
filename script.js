667c1639358fd9dd82ef4cf1f7d6e27a880c6e70
Dmytro <dmytro.2004@gmail.com>
2020-05-29 22:00:46 +0300

    Third exercise complete

--- script.js
+++ script.js
@@ -12,3 +12,14 @@ const personalMovieDB = {
     genres: [],
     privat: false
 };
+
+//3 задание 
+const film1 = prompt("Назовите один из последних просмотреных фильмов?", ""), 
+    rank1 = prompt("На сколько его оцените?", ""), 
+    film2 = prompt("Назовите один из последних просмотреных фильмов?", ""), 
+    rank2 = prompt("На сколько его оцените?", ""); 
+
+personalMovieDB.movies[film1] = rank1; 
+personalMovieDB.movies[film2] = rank2; 
+
+console.log(personalMovieDB);
