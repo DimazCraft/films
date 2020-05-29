1b0f52bf3217b0c0000e6a520a6a549df7222a20
Dmytro <dmytro.2004@gmail.com>
2020-05-29 22:00:17 +0300

    Second exercise complete

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 "use strict";
+
 //1 задание
 const numberOfMovies = prompt("Сколько фильмов ты посмотерл?", "");
 console.log(numberOfMovies);
@@ -11,14 +12,3 @@ const personalMovieDB = {
     genres: [],
     privat: false
 };
-
-//3 задание
-const film1 = prompt("Назовите один из последних просмотреных фильмов?", ""),
-     rank1 = prompt("На сколько его оцените?", ""),
-     film2 = prompt("Назовите один из последних просмотреных фильмов?", ""),
-     rank2 = prompt("На сколько его оцените?", "");
-
-personalMovieDB.movies[film1] = rank1;
-personalMovieDB.movies[film2] = rank2;
-
-console.log(personalMovieDB);
